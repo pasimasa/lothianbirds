0d72007f39efdac38cb22adbf2079018e8d064ed
Pasi Hyvonen <pasimasa@users.noreply.github.com>
2026-03-28 19:03:08 +0000

    add google tag

--- html_generator.py
+++ html_generator.py
@@ -140,6 +140,15 @@ def build_html(timestamp: str, obs_df: pd.DataFrame, duration: float,  full_stat
     return f"""<!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8QKTPZBJK7"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-8QKTPZBJK7');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lothian Bird Report</title>
